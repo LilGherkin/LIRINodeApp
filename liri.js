c60c644c4e46c06ec506b95ddabe596752f78b8f
Mark Hartman <mthartman77@gmail.com>
2019-09-15 22:22:54 -0500

    Basic file structure outlined by assignment.

---
+++ liri.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new spotify(keys.spotify);
+
+function ConcertThis(BandName) {
+
+};
+
+function SpotifyThisSong(Song) {
+
+};
+
+function MovieThis(Movie) {
+
+};
+
+function DoWhatItSays(Command) {
+};
